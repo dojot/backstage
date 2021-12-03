2be5231b64cb3061b006536f2db5be7e55d2393c
Luan Eduardo da Costa <luan1346@gmail.com>
2021-12-03 17:00:29 -0300

    Make the createDevice mutation works with GUI NX

--- src/operations/device/mutation.createDevice.js
+++ src/operations/device/mutation.createDevice.js
@@ -1,12 +1,27 @@
-const LOG = require("../../utils/Log");
-const service = require('../../services/service.device')
+const { snakeCase } = require('lodash');
+const LOG = require('../../utils/Log');
+const service = require('../../services/service.device');
+const { getObjectWithNewKeys } = require('../../utils/Object');
 
-const createDevice = async (root, device, {token}) => {
-  const {label = "", templates = [], attrs = [], certificate = ""} = device;
-
+const createDevice = async (_, device, { token }) => {
   try {
-    const {data} = await service.createDevice(token, {label, templates, attrs});
+    const {
+      label = '', templates = [], attrs = [], certificate = '',
+    } = device;
+
+    const formattedAttrs = attrs
+      ? attrs.map(attr => getObjectWithNewKeys(attr, snakeCase))
+      : attrs;
+
+    const { data } = await service.createDevice(token, {
+      label,
+      templates,
+      attrs: formattedAttrs,
+    });
+
     // Uncomment when the security service will be implemented
+    // eslint-disable-next-line no-console
+    console.log('Certificate:', certificate);
     // if (certificate) {
     //   const [device] = deviceRet.devices;
     //   await securityService.associateCertificate(token, certificate, device.id)
@@ -17,6 +32,6 @@ const createDevice = async (root, device, {token}) => {
     LOG.error(error.stack || error);
     throw error;
   }
-}
+};
 
 module.exports = createDevice;
