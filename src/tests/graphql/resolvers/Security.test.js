8dc4e65e572d7a8dbd21a19897e8ff1a995de564
Luan Eduardo da Costa <luan1346@gmail.com>
2021-12-16 14:12:46 -0300

    Fix security tests

--- src/tests/graphql/resolvers/Security.test.js
+++ src/tests/graphql/resolvers/Security.test.js
@@ -13,67 +13,68 @@ const certRequest = [{
       previous: null,
       current: {
         number: 1,
-        url: "/api/v1/certificates?page=1&limit=10"
+        url: '/api/v1/certificates?page=1&limit=10',
       },
       next: null,
       totalItems: 2,
       totalPages: 1,
-      limitPerPage: 10
+      limitPerPage: 10,
     },
     certificates: [
       {
         issuedByDojotPki: true,
         autoRegistered: false,
-        fingerprint: "63:E3:AE:0A:FF:61:A1:5E:92:FF:41:FD:7B:B3:B7:01:5F:38:3B:29:C6:FE:3B:46:13:F0:D0:62:02:48:A6:83",
-        pem: "-----BEGIN CERTIFICATE----------END CERTIFICATE-----",
+        fingerprint: '63:E3:AE:0A:FF:61:A1:5E:92:FF:41:FD:7B:B3:B7:01:5F:38:3B:29:C6:FE:3B:46:13:F0:D0:62:02:48:A6:83',
+        pem: '-----BEGIN CERTIFICATE----------END CERTIFICATE-----',
         belongsTo: {
-          device: "XXXXXX"
+          device: 'XXXXXX',
         },
-        subjectDN: "CN=e5d299, O=dojot IoT Platform",
+        subjectDN: 'CN=e5d299, O=dojot IoT Platform',
         validity: {
-          notBefore: "2021-11-26T12:26:00.000Z",
-          notAfter: "2022-11-26T12:26:00.000Z"
+          notBefore: '2021-11-26T12:26:00.000Z',
+          notAfter: '2022-11-26T12:26:00.000Z',
         },
-        tenant: "admin",
-        createdAt: "2021-11-26T12:00:00.000Z",
-        modifiedAt: "2021-11-26T12:00:00.000Z"
-      }
-    ]
-  }
+        tenant: 'admin',
+        createdAt: '2021-11-26T12:00:00.000Z',
+        modifiedAt: '2021-11-26T12:00:00.000Z',
+      },
+    ],
+  },
 }];
+
 const certResponse = [{
   pagination: {
-    page: 1,
-    total: 1,
-    hasNext: false,
+    currentPage: 1,
+    totalPages: 1,
+    hasNextPage: false,
     nextPage: null,
     limitPerPage: 10,
-    totalItems: 2
+    totalItems: 2,
   },
   certificates: [
     {
       autoRegistered: false,
       issuedByDojotPki: true,
-      subjectDN: "CN=e5d299, O=dojot IoT Platform",
-      pem: "-----BEGIN CERTIFICATE----------END CERTIFICATE-----",
-      fingerprint: "63:E3:AE:0A:FF:61:A1:5E:92:FF:41:FD:7B:B3:B7:01:5F:38:3B:29:C6:FE:3B:46:13:F0:D0:62:02:48:A6:83",
-      createdAt: "2021-11-26T12:00:00.000Z",
-      modifiedAt: "2021-11-26T12:00:00.000Z",
-      tenant: "admin",
+      subjectDN: 'CN=e5d299, O=dojot IoT Platform',
+      pem: '-----BEGIN CERTIFICATE----------END CERTIFICATE-----',
+      fingerprint: '63:E3:AE:0A:FF:61:A1:5E:92:FF:41:FD:7B:B3:B7:01:5F:38:3B:29:C6:FE:3B:46:13:F0:D0:62:02:48:A6:83',
+      createdAt: '2021-11-26T12:00:00.000Z',
+      modifiedAt: '2021-11-26T12:00:00.000Z',
+      tenant: 'admin',
       belongsTo: {
-        device: "XXXXXX"
+        device: 'XXXXXX',
       },
       validity: {
-        notAfter: "2022-11-26T12:26:00.000Z",
-        notBefore: "2021-11-26T12:26:00.000Z"
-      }
-    }
-  ]
+        notAfter: '2022-11-26T12:26:00.000Z',
+        notBefore: '2021-11-26T12:26:00.000Z',
+      },
+    },
+  ],
 }];
 
 it('Certificate - should return one certificate', () => {
   const root = {};
-  const params = {page: {number: 0, size: 5}};
+  const params = { page: { number: 0, size: 5 } };
   const context = {};
 
   axios.get.mockImplementationOnce(() => Promise.resolve(certRequest[0]));
@@ -82,4 +83,3 @@ it('Certificate - should return one certificate', () => {
     expect(output).toEqual(certResponse[0]);
   });
 });
-
