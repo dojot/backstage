a105b15e7e97b8dfd6ab8aca7a76de965eb58257
LeonamMoura <leo99.music@gmail.com>
2022-01-07 16:57:53 -0300

    refactor mutation create certificate csr

--- src/operations/security/mutation.createCertificateCSR.js
+++ src/operations/security/mutation.createCertificateCSR.js
@@ -4,22 +4,12 @@ const LOG = require("../../utils/Log");
 
 
 const createCertificateCSR = async (_, {csrPEM}, {token}) => {
-  const hashAlgorithm = 'SHA-256';
-  const signAlgorithm = 'RSASSA-PKCS1-V1_5';
-
   try {
-    const {
-      privateKeyPEM,
-      publicKeyPEM,
-    } = await helpers.generateKeyPar(signAlgorithm, hashAlgorithm);
-
     const {data: {certificatePem, certificateFingerprint}} = await service.createCertificate(token, csrPEM);
 
     return {
       certificatePem,
       certificateFingerprint,
-      privateKeyPEM,
-      publicKeyPEM
     }
 
   } catch (e) {
