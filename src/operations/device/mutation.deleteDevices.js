1fd5f7cf06188d91feae683fd155b02865151c96
LeonamMoura <leo99.music@gmail.com>
2022-02-15 09:55:43 -0300

    disassociate device on delete

--- src/operations/device/mutation.deleteDevices.js
+++ src/operations/device/mutation.deleteDevices.js
@@ -1,10 +1,18 @@
 import LOG from '../../utils/Log.js';
 import * as service from '../../services/service.device.js';
+import * as securityService from '../../services/service.security';
 
 const deleteDevices = async (_, { deviceIds }, { token }) => {
   try {
     const promises = deviceIds.map(
       async (deviceId) => {
+        const { data: certificateData } = await securityService.getAllCertificates(
+          token, undefined, undefined, deviceId,
+        );
+        const { certificates } = certificateData;
+        const certificateFingerprint = certificates[0].fingerprint;
+
+        await certificateService.disassociateCertificate(token, certificateFingerprint);
         await service.deleteDevice(token, deviceId);
       },
     );
