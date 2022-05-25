cc23248997a67c4b2855c4a2e1d4516db7faa95e
LeonamMoura <63542473+LeonamMoura@users.noreply.github.com>
2022-05-25 13:56:05 -0300

    Issue#2494: disassociate device on delete (#125)
    * disassociate device on delete
    
    * refactor: fixing the promise to disassociate each device Id from all certificates it is bound to
    
    * add promise.all on disassociateCertificates
    
    Co-authored-by: luisfelipefurlan <32846891+luisfelipefurlan@users.noreply.github.com>

--- src/operations/device/mutation.deleteDevices.js
+++ src/operations/device/mutation.deleteDevices.js
@@ -1,15 +1,29 @@
-import LOG from '../../utils/Log.js';
-import * as service from '../../services/service.device.js';
+import LOG from "../../utils/Log.js";
+import * as service from "../../services/service.device.js";
+import * as securityService from "../../services/service.security";
 
 const deleteDevices = async (_, { deviceIds }, { token }) => {
   try {
-    const promises = deviceIds.map(
-      async (deviceId) => {
-        await service.deleteDevice(token, deviceId);
-      },
-    );
-    await Promise.all(promises);
-    return 'ok';
+    const disassociateCertificatesPromise = deviceIds.map(async (deviceId) => {
+      const { data: certificateData } = await securityService
+        .getAllCertificates(
+          token,
+          undefined,
+          undefined,
+          deviceId,
+        );
+      const { certificates } = certificateData;
+      const disassociateLinkedCertificates = certificates.map(async (certificate) => {
+        await securityService.disassociateCertificate(token, certificate.fingerprint);
+      });
+      await Promise.all(disassociateLinkedCertificates);
+    });
+    const deleteDevicesPromise = deviceIds.map(async (deviceId) => {
+      await service.deleteDevice(token, deviceId);
+    });
+    await Promise.all(disassociateCertificatesPromise);
+    await Promise.all(deleteDevicesPromise);
+    return "ok";
   } catch (error) {
     LOG.error(error.stack || error);
     throw error;
