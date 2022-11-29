2259ec6a7a34986be8f4d9bd4408d9126a632a95
LeonamMoura <leo99.music@gmail.com>
2022-11-29 10:49:58 -0300

    fix codefactor issue

--- src/operations/template/mutation.deleteMultipleTemplates.js
+++ src/operations/template/mutation.deleteMultipleTemplates.js
@@ -3,13 +3,13 @@ import HandleResolverError from '../../utils/SessionValidation.js';
 
 const deleteMultipleTemplates = async (_, { templateIds }, { session, token }) => {
   try {
-    console.log(templateIds)
+    console.log(templateIds);
     const { data } = await service.deleteMultipleTemplates(token, templateIds);
 
     const deletedTemplates = data.templates;
     const templatesAssociatedDevices = data.templates_associated_devices;
-    const templatesNotFound = data.templates_not_found
-    
+    const templatesNotFound = data.templates_not_found;
+
     return { deletedTemplates, templatesAssociatedDevices, templatesNotFound };
   } catch (error) {
     HandleResolverError(session, error);
