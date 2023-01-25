9daf42437d305a789a210e9c8fadf9cfc94269a3
Rafael Souza Zanetti <rzanetti@cpqd.com.br>
2023-01-25 11:13:02 -0300

    Remove envVar sem uso e ajusta path base do x509 para limitá-lo a host:port

--- src/services/service.security.js
+++ src/services/service.security.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import config from '../config.js';
 
-const x509IdentityMgmt = config.x509_identity_mgmt;
+const x509IdentityMgmt = config.x509_identity_mgmt_url;
 
 const getHeader = token => ({
   headers: { 'content-type': 'application/json', Authorization: `Bearer ${token}` },
@@ -17,28 +17,28 @@ export const getAllCertificates = ({
   if (id || id === null) queryParams.keyVal = `belongsTo.device=${id}`;
   if (sortBy) queryParams.sortBy = sortBy;
   const queryParamsString = new URLSearchParams(queryParams).toString();
-  return axios.get(`${x509IdentityMgmt}/v1/certificates?${queryParamsString}`, getHeader(token));
+  return axios.get(`${x509IdentityMgmt}/api/v1/certificates?${queryParamsString}`, getHeader(token));
 };
 
-export const getCertificateByFingerprint = (token, fingerprint) => axios.get(`${x509IdentityMgmt}/v1/certificates/${fingerprint}`, getHeader(token));
+export const getCertificateByFingerprint = (token, fingerprint) => axios.get(`${x509IdentityMgmt}/api/v1/certificates/${fingerprint}`, getHeader(token));
 
-export const createCertificate = (token, csrPEM) => axios.post(`${x509IdentityMgmt}/v1/certificates`, { csr: csrPEM }, getHeader(token));
+export const createCertificate = (token, csrPEM) => axios.post(`${x509IdentityMgmt}/api/v1/certificates`, { csr: csrPEM }, getHeader(token));
 
-export const registerExternalCertificate = (token, certificateChain) => axios.post(`${x509IdentityMgmt}/v1/certificates`, { certificateChain }, getHeader(token));
+export const registerExternalCertificate = (token, certificateChain) => axios.post(`${x509IdentityMgmt}/api/v1/certificates`, { certificateChain }, getHeader(token));
 
-export const getCACertificate = token => axios.get(`${x509IdentityMgmt}/v1/ca`, getHeader(token));
+export const getCACertificate = token => axios.get(`${x509IdentityMgmt}/api/v1/ca`, getHeader(token));
 
-export const associateCertificate = (token, fingerprint, deviceId) => axios.patch(`${x509IdentityMgmt}/v1/certificates/${fingerprint}`, {
+export const associateCertificate = (token, fingerprint, deviceId) => axios.patch(`${x509IdentityMgmt}/api/v1/certificates/${fingerprint}`, {
   belongsTo: {
     device: deviceId,
   },
 }, getHeader(token));
 
-export const deleteCertificate = async (token, fingerprint) => axios.delete(`${x509IdentityMgmt}/v1/certificates/${fingerprint}`, getHeader(token));
+export const deleteCertificate = async (token, fingerprint) => axios.delete(`${x509IdentityMgmt}/api/v1/certificates/${fingerprint}`, getHeader(token));
 
-export const disassociateCertificate = async (token, fingerprint) => axios.delete(`${x509IdentityMgmt}/v1/certificates/${fingerprint}/belongsto`, getHeader(token));
+export const disassociateCertificate = async (token, fingerprint) => axios.delete(`${x509IdentityMgmt}/api/v1/certificates/${fingerprint}/belongsto`, getHeader(token));
 
-export const createCertificationAuthority = async (token, { caPem, allowAutoRegistration }) => axios.post(`${x509IdentityMgmt}/v1/trusted-cas`, { caPem, allowAutoRegistration }, getHeader(token));
+export const createCertificationAuthority = async (token, { caPem, allowAutoRegistration }) => axios.post(`${x509IdentityMgmt}/api/v1/trusted-cas`, { caPem, allowAutoRegistration }, getHeader(token));
 
 export const getCertificationAuthorities = async ({
   token, page, filter, sortBy,
@@ -49,10 +49,10 @@ export const getCertificationAuthorities = async ({
   if (filter && filter.caFingerprint) queryParams.caFingerprint = filter.caFingerprint;
   if (sortBy) queryParams.sortBy = sortBy;
   const queryParamsString = new URLSearchParams(queryParams).toString();
-  return axios.get(`${x509IdentityMgmt}/v1/trusted-cas?${queryParamsString}`, getHeader(token));
+  return axios.get(`${x509IdentityMgmt}/api/v1/trusted-cas?${queryParamsString}`, getHeader(token));
 };
 
-export const deleteCertificationAuthority = async (token, fingerprint) => axios.delete(`${x509IdentityMgmt}/v1/trusted-cas/${fingerprint}`, getHeader(token));
+export const deleteCertificationAuthority = async (token, fingerprint) => axios.delete(`${x509IdentityMgmt}/api/v1/trusted-cas/${fingerprint}`, getHeader(token));
 
 export const importCertificatesInBatch = async (token, {caRoot, certificates}) => {
   // to be implemented when the api is ready
